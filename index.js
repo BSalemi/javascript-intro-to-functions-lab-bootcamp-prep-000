04318f0ea883c72d44d5c3240b6bb55aed908bc3
Brian <briandsalemi@gmail.com>
2018-09-14 18:45:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function logWhisper(string){ console.log(string.toLowerCase()) }
 
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()){ return
-  'I can\'t hear you!'
+  ('I can\'t hear you!')
   }
   else if (string === string.toUpperCase()){ return
-      'YES INDEED!';
+      ('YES INDEED!');
   } 
   else if (string === "I love you, Grandma."){ return
-        'I love you, too.'
+        ('I love you, too.')
       }
 }
